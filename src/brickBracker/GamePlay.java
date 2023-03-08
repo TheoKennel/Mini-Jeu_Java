adb9a72b12d77765019af8999f55a635263e9306
TheoKennel <theokennel@gmail.com>
2023-03-08 15:55:53 +0100

    Ajout de commentaire détailler

--- src/brickBracker/GamePlay.java
+++ src/brickBracker/GamePlay.java
@@ -1,40 +1,53 @@
+// Déclaration du package pour cette classe
 package brickBracker;
 
+// Import de classes nécessaires
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+// Déclaration de la classe GamePlay qui étend la classe JPanel et implémente les interfaces KeyListener et ActionListener
 public class GamePlay extends JPanel implements KeyListener, ActionListener {
+
+    // Variables membres privées pour stocker l'état du jeu
     private boolean play = false;
-    private  int score = 0;
+    private int score = 0;
     private int totalBricks = 21;
 
+    // Timer pour gérer les mouvements de la balle
     private Timer time;
     private int delay = 8;
 
+    // Positions de départ de la balle et du joueur
     private int playerX = 310;
     private int ballposX = 120;
     private int ballposY = 350;
+
+    // Directions initiales de la balle
     private int ballXdir = -1;
     private int ballYdir = -2;
 
+    // Implémentation de la méthode actionPerformed de l'interface ActionListener
     @Override
     public void actionPerformed(ActionEvent e) {
 
     }
 
+    // Implémentation de la méthode keyTyped de l'interface KeyListener
     @Override
     public void keyTyped(KeyEvent e) {
 
     }
 
+    // Implémentation de la méthode keyPressed de l'interface KeyListener
     @Override
     public void keyPressed(KeyEvent e) {
 
     }
 
+    // Implémentation de la méthode keyReleased de l'interface KeyListener
     @Override
     public void keyReleased(KeyEvent e) {
 
--- src/brickBracker/main.java
+++ src/brickBracker/main.java
@@ -1,16 +1,36 @@
+
 package brickBracker;
 
 import javax.swing.*;
 
 public class main {
+
+    // Cette ligne définit une méthode publique et statique nommée "main" qui prend en entrée un tableau de chaînes de caractères "args"
     public static void main(String[] args) {
+
+        // Création d'un objet JFrame appelé "obj"
         JFrame obj = new JFrame();
+
+        // Création d'un objet GamePlay appelé "gamePlay"
         GamePlay gamePlay = new GamePlay();
+
+        // définit les dimensions de la fenêtre JFrame "obj"
         obj.setBounds(10, 10, 700, 600);
-        obj.setTitle("Breakout Ball");;
+
+        // définit le titre de la fenêtre JFrame "obj"
+        obj.setTitle("Breakout Ball");
+
+        //  empêche la fenêtre JFrame "obj" d'être redimensionnée
         obj.setResizable(false);
+
+        // rend la fenêtre JFrame "obj" visible à l'utilisateur
         obj.setVisible(true);
+
+        // Cette ligne définit l'action à effectuer lorsque l'utilisateur ferme la fenêtre JFrame "obj"
         obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Cette ligne ajoute l'objet GamePlay "gamePlay" à la fenêtre JFrame "obj"
         obj.add(gamePlay);
     }
 }
+
