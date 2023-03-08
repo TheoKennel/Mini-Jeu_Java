0d7fe3c4fdfade94c2997125539cc031edc4e3f1
TheoKennel <theokennel@gmail.com>
2023-03-08 16:33:57 +0100

    Ajout du style Background, border, paddle et balle, paint

--- src/brickBracker/GamePlay.java
+++ src/brickBracker/GamePlay.java
@@ -3,6 +3,7 @@ package brickBracker;
 
 // Import de classes nécessaires
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -28,6 +29,34 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
     // Directions initiales de la balle
     private int ballXdir = -1;
     private int ballYdir = -2;
+
+    public GamePlay() {
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+        timer = new Timer(delay, this);
+        timer.start();
+    }
+
+    public void paint(Graphics g) {
+        // Background
+        g.setColor(Color.BLACK);
+        g.fillRect(1,1,692, 592);
+
+        // borders
+        g.setColor(Color.yellow);
+        g.fillRect(0,0,3,592);
+        g.fillRect(0,0,692,3);
+        g.fillRect(691,0,3,592);
+
+        // Paddle (barre)
+        g.setColor(Color.green);
+        g.fillRect(playerX, 550, 100, 8);
+
+        // Balle
+        g.setColor(Color.yellow);
+        g.fillRect(ballposX, ballposY, 20, 20);
+    }
 
     // Implémentation de la méthode actionPerformed de l'interface ActionListener
     @Override
